fbbc1f52eb10d43bd970dbff4153b3a84959d661
mojvaf <mvafa44@gmail.com>
2021-02-01 14:55:11 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/' , (req,res)=> res.json({ msg: 'welcome to the Contact keeper APl ...'}))
+
+// Define Router
+app.use('/api/users', require('./routers/users'))
+app.use('/api/auth' , require('./routers/auth'))
+app.use('/api/contacts', require('./routers/contacts'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT , ()=> console.log(`server started on prot ${PORT}`))
