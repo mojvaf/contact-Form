c82d7ad04bb92f5dc1efec6f99c5b52416a3bc38
mojvaf <mvafa44@gmail.com>
2021-02-02 23:47:32 +0100

    delete added

--- routers/contacts.js
+++ routers/contacts.js
@@ -93,7 +93,26 @@ router.put('/:id', auth, async(req, res)=>{
 // @desc   Delete contact
 // @ access Private
 
-router.delete('/:id', (req, res)=>{
-    res.send('delete contact')
+router.delete('/:id', auth,async(req, res)=>{
+    
+    try {
+        let contact =await Contact.findById(req.params.id)
+   
+        if(!contact){
+            return res.status(404).json({msg: 'Contact not find'})
+        }
+   
+        //make sure user owns contact
+        if(contact.user.toString() !== req.user.id){
+            return res.status(401).json({msg: 'Not authorized'})
+        }
+   
+        await Contact.findByIdAndRemove(req.params.id)
+        res.json({message: 'Contact removed'})
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send('server error from contact')
+    }
+   
 })
 module.exports = router;
