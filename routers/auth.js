62739e2149f2ccb40ca3b59120bbc68d157e2112
mojvaf <mvafa44@gmail.com>
2021-02-02 15:39:19 +0100

    auth middle ware added

--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
+const auth = require('../middleware/auth')
 const {check, validationResult} = require('express-validator/check')
 const User = require('../models/User')
 
@@ -12,8 +13,14 @@ const User = require('../models/User')
 // @ access   Private
 
 //validate the log and user
-router.get('/' , (req,res)=>{
-    res.send('Get logged in user')
+router.get('/' , auth, async(req,res)=>{
+    try{
+      const user = await User.findById(req.user.id).select('-password')
+      res.json(user)
+    }catch(error){
+       console.error(err.message)
+       res.status(500).send('server Error')
+    }
 })
 
 // @route   POST api/auth
