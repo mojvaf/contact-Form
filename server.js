d4550aead2fc507645ebbb069e91871b0eabfa96
mojvaf <mvafa44@gmail.com>
2021-03-03 20:20:52 +0100

    final

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const express = require('express')
 const connectDB = require('./config/db')
-const path = require('path')
 const app = express();
+const path = require('path')
+
 
 // connect Database
 connectDB()
 // Init Middleware(accept data)
 app.use(express.json({ extended: false }))
 
-
+app.get('/' , (req, res)=>
+   res.json({ msg: 'welcome'})
+ )
 
 // Define Router
 app.use('/api/users', require('./routers/users'))
 app.use('/api/auth' , require('./routers/auth'))
 app.use('/api/contacts', require('./routers/contacts'))
 
-// Server static assets in production
- if(process.env.NODE_ENV === 'production'){
-      // set static folder
-   app.use(express.static('client/build'))
 
-   app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')))
- }
+//Server static assets in production
+if(process.env.NODE_ENV === 'production'){
+  // set static folder 
+  app.use(express.static('client/build'))
+
+  app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
   
 
 const PORT = process.env.PORT || 5000
